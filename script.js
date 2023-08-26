065f083a0605dd733ed1f1a36e86ecdda8c92fd7
Mecit Bedel <mjt.bdl@gmail.com>
2023-08-26 14:31:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 // Selecting elements
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.querySelector('#current--0'); // # is for id
+const current1El = document.querySelector('#current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -12,6 +15,8 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+let currentScore = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -23,4 +28,13 @@ btnRoll.addEventListener('click', function () {
   diceEl.src = `img/dice-${dice}.png`;
 
   // 3. Check for rolled 1: if true, switch to next player
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; // Change later
+  } else {
+    // Switch to next player
+    current0El.textContent = 0;
+    currentScore = 0;
+  }
 });
