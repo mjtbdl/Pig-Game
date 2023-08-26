7db5764b766d7710746fbeeaa7b62f2a5e046f0d
Mecit Bedel <mjt.bdl@gmail.com>
2023-08-26 14:40:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting elements
+const player0El = document.querySelector('.player--0'); // . is for class
+const player1El = document.querySelector('.player--1'); //
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.querySelector('#current--0'); // # is for id
