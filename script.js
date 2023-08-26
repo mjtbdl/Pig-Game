c7618ec0f1e44415f4deac21f27f9a0f8a59ad8e
Mecit Bedel <mjt.bdl@gmail.com>
2023-08-26 15:29:41 +0300

    update

--- script.js
+++ script.js
@@ -13,11 +13,13 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+let scores, currentScore, activePlayer, playing; // global variables
+
 const init = function () {
-  const scores = [0, 0];
-  let currentScore = 0;
-  let activePlayer = 0;
-  let playing = true; // state variable
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true; // state variable
 
   score0El.textContent = 0;
   score1El.textContent = 0;
@@ -93,6 +95,4 @@ btnHold.addEventListener('click', function () {
 });
 
 // Resetting the game
-btnNew.addEventListener('click', function () {
-  init();
-});
+btnNew.addEventListener('click', init);
